8267eb9b25c5aa82db90ef9fd5123268c2e38d7d
chasemydrea <2196018156@qq.com>
2022-12-04 17:58:20 +0800

    第一次提交个人信息这个Java文件

---
+++ TestModule01/src/com/msd/Person.java
@@ -0,0 +1,27 @@
+package com.msd;
+
+public class Person {
+    public static void main(String[]args){
+      personInformation("小玉");
+      String name = personInformation("小王");
+      System.out.println(personInformation("小玉"));
+
+    }
+       public static String personInformation(String name ){
+       String name1 = "小玉";
+       //System.out.println(name);
+       //System.out.println();
+       //System.out.println();
+      // System.out.println();
+       return name;
+
+    }
+
+
+
+
+
+
+
+
+}
